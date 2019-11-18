d8766c628dc748c4b9d8d39ef9cb1bb6f65854e5
rogerwelin <roger.welin@icloud.com>
2019-11-18 10:55:42 +0100

    readme

--- tmpl-readme.go
+++ tmpl-readme.go
@@ -3,12 +3,14 @@ package main
 const reamdeFile = `
 ## API GW Project
 
+### Set up the Environment
+To create the API GW project you will need an AWS account and generate API keys for your IAM user. Run *aws configure* after installing the awscli. Then you you will need to create a new S3 bucket that will be used for storing deployment files.
+
 ### Install Dependencies
 You need the following dependencies to be able to build and deploy the api project:
 * A working Pyhton installation
 * awscli
 * sam (serverless application model)
-* create a new S3-bucket to store deployment files
 
 **Install awscli and SAM**  
 ` + "```" + `bash
@@ -25,6 +27,8 @@ $ sam package --template-file apigw.yml  --output-template-file out.yaml --s3-bu
 ` + "```" + `bash
 $ sam deploy --template-file ./out.yaml --stack-name your-api-project --capabilities CAPABILITY_IAM 
 ` + "```" + `
+
+Go to the AWS console > Cloudformation. Make sure the Cloudformations stack finishes. Take a look at the output to get the URL of your newly created API project. Either curl the address at the /hello endpoint or run the endpoint directly in the API Gateway console.
 
 
 `
